335039fc72f7767ab00ffe5377b680dd8836e694
ewurapoms <mzpomaa35@gmail.com>
2023-12-08 07:39:30 -0800

    Mandatory tasks

--- web_dynamic/3-hbnb.py
+++ web_dynamic/3-hbnb.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """ Starts a Flash Web Application """
+from flask import Flask, render_template
 from models import storage
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from os import environ
-from flask import Flask, render_template
 import uuid
 app = Flask(__name__)
 # app.jinja_env.trim_blocks = True
@@ -15,7 +15,7 @@ app = Flask(__name__)
 
 @app.teardown_appcontext
 def close_db(error):
-    """ Remove the current SQLAlchemy Session """
+    """ ends the current SQLAlchemy Session """
     storage.close()
 
 
@@ -42,5 +42,4 @@ def hbnb():
 
 
 if __name__ == "__main__":
-    """ Main Function """
     app.run(host='0.0.0.0', port=5000)
